62ec9060bde09c94695d6d6cfc073c9cd256a252
Daiki Iizuka <da.iz.mg@gmail.com>
2018-10-21 01:05:05 +0900

    slim phys-png (#1)
    * ptr操作を整理
    
    * attributeChangedCallback
    
    * shadowRoot.appendChild

--- src/phys-png.js
+++ src/phys-png.js
@@ -1,11 +1,13 @@
 /* Physical Portable Network Graphics */
 class PhysPng extends HTMLElement {
-  constructor () {
-    super()
+  constructor (...props) {
+    super(...props)
     this.pngSignature = '89 50 4E 47 0D 0A 1A 0A'
     this.width = 0
     this.height = 0
     this.dpi = 72
+    this.byteArray = null
+    this.ptr = 0
     this.render()
   }
 
@@ -15,19 +17,30 @@ class PhysPng extends HTMLElement {
   setImgSize () {
     if (this.dpi === 72) return
     this.img.style.width = `${this.width / (this.dpi / 72)}px`
-    // this.img.style.height = `${this.height / (this.dpi / 72)}px`
   }
 
-  padZero2digits (str) {
-    return (`00${str.toUpperCase()}`).slice('-2')
+  padZero (str, digits) {
+    const zeros = '0'.repeat(digits)
+    return (`${zeros}${str.toUpperCase()}`).slice(`-${digits}`)
   }
 
-  padZero8digits (str) {
-    return (`00000000${str.toUpperCase()}`).slice('-8')
+  readBytes (bytes) {
+    const res = this.byteArray.slice(this.ptr, this.ptr + bytes)
+    this.ptr += bytes
+    // Convert Uint8Array to Array
+    return Array.from(res)
   }
 
-  async attributeChangedCallback (attr, oldVal, newval) {
-    const srcUrl = this.getAttribute('src')
+  toHex (value, digits) {
+    return this.padZero(value.toString(16), digits)
+  }
+
+  toBin (value, digits) {
+    return this.padZero(value.toString(2), digits)
+  }
+
+  async attributeChangedCallback (attr, oldVal, newVal) {
+    const srcUrl = newVal
     const followdpi = this.getAttribute('followdpi')
     if (!this.img || !srcUrl) return
     if (followdpi === null) {
@@ -39,7 +52,6 @@ class PhysPng extends HTMLElement {
     xhr.open('GET', srcUrl, true)
     xhr.responseType = 'arraybuffer'
     xhr.onload = () => {
-      console.log(srcUrl)
       const arrayBuffer = xhr.response
       if (arrayBuffer) {
         this.readChunks(arrayBuffer)
@@ -54,110 +66,74 @@ class PhysPng extends HTMLElement {
     xhr.send(null)
   }
 
-  readIHDR (byteArray, ptr) {
-    // Length
-    ptr += 4
-
-    // Chunk Type (49 48 44 52)
-    const chunkType = [
-      byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-    ].map(v => this.padZero2digits(v.toString(16)))
-    ptr += chunkType.length
+  readIHDR () {
+    // Length, ChunkType
+    this.ptr += (4 + 4)
 
     // Width
-    const width = [
-      byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-    ].map(v => this.padZero8digits(v.toString(2)))
+    const width = this.readBytes(4).map(v => this.toBin(v, 8))
     this.width = parseInt(width.join(''), 2)
-    console.log('width', this.width)
-    ptr += width.length
 
     // Height
-    const height = [
-      byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-    ].map(v => this.padZero8digits(v.toString(2)))
+    const height = this.readBytes(4).map(v => this.toBin(v, 8))
     this.height = parseInt(height.join(''), 2)
-    console.log('height', this.height)
-    ptr += height.length
 
     // ビット深度, カラータイプ, 圧縮手法, フィルター手法, インターレース手法, CRC
-    ptr += (1 + 1 + 1 + 1 + 1 + 4)
-
-    return ptr
+    this.ptr += (1 + 1 + 1 + 1 + 1 + 4)
   }
 
-  readpHYs (byteArray, ptr) {
-    const pixelsPerUnitXAxis = parseInt([
-      byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-    ].map(v => this.padZero8digits(v.toString(2))).join(''), 2)
-    const pixelsPerUnitYAxis = parseInt([
-      byteArray[ptr + 4], byteArray[ptr + 5], byteArray[ptr + 6], byteArray[ptr + 7]
-    ].map(v => this.padZero8digits(v.toString(2))).join(''), 2)
-    const unitSpecifier = byteArray[ptr + 8] // meters
+  readpHYs () {
+    const pixelsPerUnitXAxis = parseInt(
+      this.readBytes(4).map(v => this.toBin(v, 8)).join(''), 2)
+    const pixelsPerUnitYAxis = parseInt(
+      this.readBytes(4).map(v => this.toBin(v, 8)).join(''), 2)
+    const unitSpecifier = this.readBytes(1) // meters
     if (unitSpecifier > 0) {
       // dots per inch を計算する
       this.dpi = Math.floor(
         Math.max(pixelsPerUnitXAxis, pixelsPerUnitYAxis) / (unitSpecifier * 39.3))
     }
-    console.log({pixelsPerUnitXAxis, pixelsPerUnitYAxis, unitSpecifier, dpi: this.dpi})
   }
 
-  isPngFile (byteArray) {
-    /* PNG ファイルシグネチャ (89 50 4E 47 0D 0A 1A 0A) */
-    const signature = [
-      byteArray[0], byteArray[1], byteArray[2], byteArray[3],
-      byteArray[4], byteArray[5], byteArray[6], byteArray[7]
-    ].map(v => this.padZero2digits(v.toString(16)))
+  isPngFile () {
+    const signature = this.readBytes(8).map(v => this.toHex(v, 2))
     return signature.join(' ') === this.pngSignature
   }
 
   readChunks (arrayBuffer) {
-    const byteArray = new Uint8Array(arrayBuffer)
-    let ptr = 0
-
+    this.byteArray = new Uint8Array(arrayBuffer)
     /* PNGファイルシグネチャ確認 */
-    if (!this.isPngFile(byteArray)) return
-    ptr += 8
-
+    if (!this.isPngFile()) return
     /* IHDRチャンク */
-    ptr = this.readIHDR(byteArray, ptr)
+    this.readIHDR()
 
     while (true) {
-      if (ptr >= byteArray.length) break
+      if (this.ptr >= this.byteArray.length) break
 
-      let chunkLength = [
-        byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-      ].map(v => this.padZero8digits(v.toString(2)))
+      let chunkLength = this.readBytes(4).map(v => this.toBin(v, 8))
       chunkLength = parseInt(chunkLength.join(''), 2)
-      ptr += 4
-      const chunkType = new TextDecoder('utf-8').decode(new Uint8Array([
-        byteArray[ptr], byteArray[ptr + 1], byteArray[ptr + 2], byteArray[ptr + 3]
-      ]))
-      ptr += 4
+
+      const chunkType = new TextDecoder('utf-8')
+        .decode(new Uint8Array(this.readBytes(4)))
+
       // Chunk Data
+      if (chunkType === 'IDAT' || chunkType === 'IEND') break
       switch (chunkType) {
-        case 'pHYs': {
-          this.readpHYs(byteArray, ptr)
+        case 'pHYs':
+          this.readpHYs()
           break
-        }
-        case 'IDAT':
-        case 'IEND': {
-          break
-        }
-        default: {
-          console.log('length:', chunkLength, 'type:', chunkType)
-        }
+        default:
+          this.ptr += chunkLength
       }
-      ptr += chunkLength
       // CRC
-      ptr += 4
+      this.ptr += 4
     }
   }
 
   render () {
     const shadowRoot = this.attachShadow({mode: 'open'})
-    shadowRoot.innerHTML = `<img class='img' />`
-    this.img = shadowRoot.querySelector('img.img')
+    this.img = document.createElement('img')
+    shadowRoot.appendChild(this.img)
   }
 }
 
